acae1bd8eda588ddbbc7aa920ab0cb07b71f1210
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2023-11-19 08:46:59 -0600

    added an array of questions for the readme generator in index.js

--- index.js
+++ index.js
@@ -3,14 +3,60 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-    // Add other questions for the README content
-  }
-];
+  const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a brief description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How is your project used?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license does your project have?',
+      choices: ['MIT', 'Apache', 'GPL', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'How can others contribute to your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How do you run tests for your project?',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: 'What is your name (as the author of the project)?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ];
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
